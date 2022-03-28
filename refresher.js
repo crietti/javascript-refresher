108ed0e960cc7108b2557fb8c19014f1fe5c1c7b
crietti <crietti22@penguinhall.org>
2022-03-28 13:17:23 -0400

    leaving for drama

--- refresher.js
+++ refresher.js
@@ -1,2 +1,29 @@
-console.log("Hello world...");
-// comment
+// console.log("Hello world...");
+
+let message = "How are you today?"
+// console.log(message);
+
+// //I am here
+
+let greeting = "Hello"
+let name = "Sue"
+// console.log(greeting + " " + name + "!")
+
+let age = 13
+// console.log("You are " + age)
+// console.log("In 10 years you will be " + (age + 10))
+
+//THIS IS PART 2
+
+// if (name == "Bob"){
+//     console.log("I knew it was you, " + name + "!")
+// } else {
+//     console.log("Hey " + name + ", you're not Bob!")
+// }
+
+let iAmWearingABelt = true;
+let iAmWearingGlasses = true;
+
+if (iAmWearingABelt && iAmWearingGlasses) {
+    console.log("Probably not Mr. Gorton")
+}

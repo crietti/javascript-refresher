e2e7a202bcfd8b666dd48ece292a3bab4ae4bec9
crietti <crietti22@penguinhall.org>
2022-03-30 11:14:20 -0400

    deck of cards

---
+++ deck-of-cards/deck-of-cards.js
@@ -0,0 +1,14 @@
+listCards("Diamonds");
+listCards("Hearts");
+listCards("Spades");
+listCards("Clubs");
+
+function listCards(suit) {
+    console.log("Ace of " + suit);
+    for (let i = 2; i < 11; i++) {
+        console.log(i + " of " + suit);
+    }
+    console.log("Jack of " + suit)
+    console.log("Queen of " + suit)
+    console.log("King of " + suit)
+}

8f1084438086cdc4a2f09e888f9b6c7ab70a0ccb
crietti <crietti22@penguinhall.org>
2022-04-10 14:52:34 -0400

    stuck

--- real-javascript/myScript.js
+++ real-javascript/myScript.js
@@ -7,3 +7,7 @@ function change() {
     let firstName = document.getElementById("nameInput").value;
     pElement.innerHTML = "Hello " + firstName;
 }
+
+function changeColor(){
+    let color = document.getElementById("mouseOver").style.backgroundColor = "red"
+}
